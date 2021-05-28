002dfcc774c5f65b5880d016791bcfedc89fea13
Joe Keenan <joe@flyingdiver.com>
2021-05-28 14:46:30 -0400

    Retry API calls that return error (once)

--- MyQ.indigoPlugin/Contents/Server Plugin/wrapper.py
+++ MyQ.indigoPlugin/Contents/Server Plugin/wrapper.py
@@ -2,6 +2,7 @@
 import asyncio
 import sys
 import json
+import time
 
 from aiohttp import ClientSession
 
@@ -22,6 +23,118 @@ def msg_write(msg):
     sys.stdout.write(u"{}\n".format(msg))
     sys.stdout.flush()
 
+async def process(api, request):
+
+    cmd = request['cmd']
+    if cmd == 'stop':
+        return True
+
+    elif cmd == 'accounts':
+        for account in api.accounts:
+            msg_write(json.dumps({'msg': 'account', 'id': account, 'name': api.accounts[account]}))
+        return True
+
+    elif cmd == 'devices':
+        for device_id in api.devices:
+            device = api.devices[device_id]
+            msg_write(json.dumps({'msg': 'device', 'id': device_id, 'props':device.device_json}))
+        return True
+                
+    elif cmd == 'open':
+        device = api.devices[request['id']]
+        
+        if not device.open_allowed:
+            msg_write(json.dumps({'msg': 'status', 'status': f"Opening of garage door {device.name} is not allowed."}))
+            return True
+            
+        if device.state == STATE_OPEN:
+            msg_write(json.dumps({'msg': 'status', 'status': f"Garage door {device.name} is already open"}))
+            return True
+            
+        try:
+            wait_task = await device.open(wait_for_state=False)
+        except MyQError as err:
+            msg_write(json.dumps({'msg': 'error', 'error': f"Error when trying to open {device.name}: {str(err)}"}))
+            device = api.devices[device.device_id]
+            msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
+            return False
+                 
+        msg_write(json.dumps({'msg': 'status', 'status': f"Device {device.name} is {device.state}"}))
+       
+        if not await wait_task:
+            msg_write(json.dumps({'msg': 'status', 'status': f"Failed to close garage door {device.name}."}))
+        device = api.devices[device.device_id]
+        msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
+        return True
+            
+    elif cmd == 'close':
+        device = api.devices[request['id']]
+
+        if not device.close_allowed:
+            msg_write(json.dumps({'msg': 'status', 'status': f"Closing of garage door {device.name} is not allowed."}))
+            return True
+            
+        if device.state == STATE_CLOSED:
+            msg_write(json.dumps({'msg': 'status', 'status': f"Garage door {device.name} is already closed"}))
+            return True
+            
+        try:
+            wait_task = await device.close(wait_for_state=False)
+        except MyQError as err:
+            msg_write(json.dumps({'msg': 'error', 'error': f"Error when trying to close {device.name}: {str(err)}"}))
+            device = api.devices[device.device_id]
+            msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
+            return False
+            
+        msg_write(json.dumps({'msg': 'status', 'status': f"Device {device.name} is {device.state}"}))
+
+        if not await wait_task:
+            msg_write(json.dumps({'msg': 'status', 'status': f"Failed to close garage door {device.name}."}))
+        device = api.devices[device.device_id]
+        msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
+        return True
+
+
+    elif cmd == 'turnon':
+        device = api.devices[request['id']]
+        msg_write(json.dumps({'msg': 'status', 'status': f"Turning lamp {device.name} on"}))
+        
+        try:
+            wait_task = await device.turnon(wait_for_state=False)
+        except MyQError as err:
+            msg_write(json.dumps({'msg': 'error', 'error': f"Error when trying to turn on {device.name}: {str(err)}"}))
+            device = api.devices[device.device_id]
+            msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
+            return False
+                
+        msg_write(json.dumps({'msg': 'status', 'status': f"Device {device.name} is {device.state}"}))
+
+        if not await wait_task:
+            msg_write(json.dumps({'msg': 'status', 'status': f"Failed to turn on lamp {device.name}."}))
+        device = api.devices[device.device_id]
+        msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
+        return True
+
+             
+    elif cmd == 'turnoff':
+        device = api.devices[request['id']]
+        msg_write(json.dumps({'msg': 'status', 'status': f"Turning lamp {device.name} off"}))
+        try:
+            wait_task = await device.turnoff(wait_for_state=False)
+        except RequestError as err:
+            msg_write(json.dumps({'msg': 'error', 'error': f"Error when trying to turn off {device.name}: {str(err)}"}))
+            device = api.devices[device.device_id]
+            msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
+            return False
+
+        msg_write(json.dumps({'msg': 'status', 'status': f"Device {device.name} is {device.state}"}))
+
+        if not await wait_task:
+            msg_write(json.dumps({'msg': 'status', 'status': f"Failed to turn off lamp {device.name}."}))
+        device = api.devices[device.device_id]
+        msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
+        return True
+
 async def main(args) -> None:
 
     async with ClientSession() as websession:
@@ -41,108 +154,12 @@ async def main(args) -> None:
 
             request = json.loads(line.rstrip())
             msg_write(json.dumps({'msg': 'echo', 'request': request}))
-            cmd = request['cmd']
             
-            if cmd == 'stop':
-                break
-
-            elif cmd == 'accounts':
-                for account in api.accounts:
-                    msg_write(json.dumps({'msg': 'account', 'id': account, 'name': api.accounts[account]}))
-
-            elif cmd == 'devices':
-                for device_id in api.devices:
-                    device = api.devices[device_id]
-                    msg_write(json.dumps({'msg': 'device', 'id': device_id, 'props':device.device_json}))
-                        
-            elif cmd == 'open':
-                device = api.devices[request['id']]
+            if not await process(api, request):     # retry once
+                msg_write(json.dumps({'msg': 'status', 'status': "Retrying last command"}))
+                time.sleep(1)
+                await process(api, request)
                 
-                if not device.open_allowed:
-                    msg_write(json.dumps({'msg': 'status', 'status': f"Opening of garage door {device.name} is not allowed."}))
-                    continue
-                    
-                if device.state == STATE_OPEN:
-                    msg_write(json.dumps({'msg': 'status', 'status': f"Garage door {device.name} is already open"}))
-                    continue
-                    
-                try:
-                    wait_task = await device.open(wait_for_state=False)
-                except MyQError as err:
-                    msg_write(json.dumps({'msg': 'error', 'error': f"Error when trying to open {device.name}: {str(err)}"}))
-                    device = api.devices[device.device_id]
-                    msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
-                    continue
-                         
-                msg_write(json.dumps({'msg': 'status', 'status': f"Device {device.name} is {device.state}"}))
-               
-                if not await wait_task:
-                    msg_write(json.dumps({'msg': 'status', 'status': f"Failed to close garage door {device.name}."}))
-                device = api.devices[device.device_id]
-                msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
-                    
-            elif cmd == 'close':
-                device = api.devices[request['id']]
-
-                if not device.close_allowed:
-                    msg_write(json.dumps({'msg': 'status', 'status': f"Closing of garage door {device.name} is not allowed."}))
-                    continue
-                    
-                if device.state == STATE_CLOSED:
-                    msg_write(json.dumps({'msg': 'status', 'status': f"Garage door {device.name} is already closed"}))
-                    continue
-                    
-                try:
-                    wait_task = await device.close(wait_for_state=False)
-                except MyQError as err:
-                    msg_write(json.dumps({'msg': 'error', 'error': f"Error when trying to close {device.name}: {str(err)}"}))
-                    device = api.devices[device.device_id]
-                    msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
-                    continue
-                    
-                msg_write(json.dumps({'msg': 'status', 'status': f"Device {device.name} is {device.state}"}))
-
-                if not await wait_task:
-                    msg_write(json.dumps({'msg': 'status', 'status': f"Failed to close garage door {device.name}."}))
-                device = api.devices[device.device_id]
-                msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
-
-
-            elif cmd == 'turnon':
-                device = api.devices[request['id']]
-                msg_write(json.dumps({'msg': 'status', 'status': f"Turning lamp {device.name} on"}))
-                
-                try:
-                    wait_task = await device.turnon(wait_for_state=False)
-                except MyQError as err:
-                    msg_write(json.dumps({'msg': 'error', 'error': f"Error when trying to turn on {device.name}: {str(err)}"}))
-                    device = api.devices[device.device_id]
-                    msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
-                         
-                msg_write(json.dumps({'msg': 'status', 'status': f"Device {device.name} is {device.state}"}))
-
-                if not await wait_task:
-                    msg_write(json.dumps({'msg': 'status', 'status': f"Failed to turn on lamp {device.name}."}))
-                device = api.devices[device.device_id]
-                msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
-
-                     
-            elif cmd == 'turnoff':
-                device = api.devices[request['id']]
-                msg_write(json.dumps({'msg': 'status', 'status': f"Turning lamp {device.name} off"}))
-                try:
-                    wait_task = await device.turnoff(wait_for_state=False)
-                except RequestError as err:
-                    msg_write(json.dumps({'msg': 'error', 'error': f"Error when trying to turn off {device.name}: {str(err)}"}))
-                    device = api.devices[device.device_id]
-                    msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
-
-                msg_write(json.dumps({'msg': 'status', 'status': f"Device {device.name} is {device.state}"}))
-
-                if not await wait_task:
-                    msg_write(json.dumps({'msg': 'status', 'status': f"Failed to turn off lamp {device.name}."}))
-                device = api.devices[device.device_id]
-                msg_write(json.dumps({'msg': 'device', 'id': device.device_id, 'props':device.device_json}))
 
     
 # actual start of the program
